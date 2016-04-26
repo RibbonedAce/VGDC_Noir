5a45cfadc37d8c8cbcdaca54947d60220c7d04d6
thetylerace13 <tylergemora@yahoo.com>
2016-04-25 20:21:56 -0700

    Project initialized

---
+++ VGDC_Noir_Copy/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed;
+    public int jumpHeight;
+    private bool onGround = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed * Input.GetAxis("Horizontal"));
+        }
+
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpHeight);
+            onGround = false;
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+	}
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            onGround = true;
+        }
+    }
+}
---
+++ VGDC_Noir_Copy/Assets/Scripts/PlayerTracking.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTracking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+
+        transform.position = new Vector3(player.transform.position.x, 0, -10);
+	}
+}
