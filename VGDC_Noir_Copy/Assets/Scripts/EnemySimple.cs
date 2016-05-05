b0f215411c0a57213e8c2f8d6caccabc60995af0
thetylerace13 <tylergemora@yahoo.com>
2016-05-04 21:35:44 -0700

    Started enemy; was packed today, so not much done.

---
+++ VGDC_Noir_Copy/Assets/Scripts/EnemySimple.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySimple : MonoBehaviour {
+    private bool isCalm = true;
+    public float paceLength;
+    public string direciton;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isCalm)
+        {
+            StartCou
+        }
+	    
+	}
+
+    IEnumerator pace (string direction, float time)
+    {
+        if (direction == "left")
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+
+            yield return new WaitForSeconds(time);
+
+            transform.Translate(Vector3.right * Time.deltaTime);
+
+            yield return new WaitForSeconds(time);
+        }
+
+        if (direction == "right")
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+
+            yield return new WaitForSeconds(time);
+
+            transform.Translate(Vector3.left * Time.deltaTime);
+
+            yield return new WaitForSeconds(time);
+        }
+
+    }
+}
