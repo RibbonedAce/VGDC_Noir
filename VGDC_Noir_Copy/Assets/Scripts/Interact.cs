543c97685a05571e6087fc0d09249f8fe6ef8944
thetylerace13 <tylergemora@yahoo.com>
2016-05-06 11:45:04 -0700

    Can interact with 'e'. Can switch between shadow and player with 'f'.

---
+++ VGDC_Noir_Copy/Assets/Scripts/Interact.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interact : MonoBehaviour {
+    public bool canInteract = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonDown("Interact") && canInteract)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag("PlayerCharacter"))
+        {
+            canInteract = true;
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D other)
+    {
+        if (other.CompareTag("PlayerCharacter"))
+        {
+            canInteract = false;
+        }
+    }
+}
