8256d3676f142b3445ceb44791d3a2f2824544ef
thetylerace13 <tylergemora@yahoo.com>
2016-05-05 19:48:16 -0700

    Added enemy

--- VGDC_Noir_Copy/Assets/Scripts/EnemySimple.cs
+++ VGDC_Noir_Copy/Assets/Scripts/EnemySimple.cs
@@ -4,47 +4,54 @@ using System.Collections;
 public class EnemySimple : MonoBehaviour {
     private bool isCalm = true;
     public float paceLength;
-    public string direciton;
+    private float paceTime;
+    public bool startsLeft;
+    private bool goingLeft;
+    public float speed;
 
 	// Use this for initialization
 	void Start ()
     {
-	    
-	}
+        paceTime = paceLength;
+        goingLeft = startsLeft;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        Animator _animator = gameObject.GetComponent<Animator>();
+
+        VisionCone cone = gameObject.GetComponentInChildren<VisionCone>();
+
+        if (cone.detectsPlayer)
+        {
+            isCalm = false;
+        }
+
         if (isCalm)
         {
-            StartCou
+            if (paceTime >= 0)
+            {
+                paceTime -= Time.deltaTime;
+            }
+            else
+            {
+                goingLeft = !goingLeft;
+                paceTime = paceLength;
+            }
+
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+            if (goingLeft)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 180);
+            }
         }
-	    
+
+        _animator.SetBool("Calm", isCalm);
 	}
-
-    IEnumerator pace (string direction, float time)
-    {
-        if (direction == "left")
-        {
-            transform.Translate(Vector3.left * Time.deltaTime);
-
-            yield return new WaitForSeconds(time);
-
-            transform.Translate(Vector3.right * Time.deltaTime);
-
-            yield return new WaitForSeconds(time);
-        }
-
-        if (direction == "right")
-        {
-            transform.Translate(Vector3.right * Time.deltaTime);
-
-            yield return new WaitForSeconds(time);
-
-            transform.Translate(Vector3.left * Time.deltaTime);
-
-            yield return new WaitForSeconds(time);
-        }
-
-    }
 }
---
+++ VGDC_Noir_Copy/Assets/Scripts/VisionCone.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionCone : MonoBehaviour {
+    public bool detectsPlayer = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	      
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("triggered");
+
+        if (other.CompareTag("PlayerCharacter"))
+        {
+            detectsPlayer = true;
+        }
+    }
+}
