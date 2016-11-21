74889f30bbff3a45a590c88c4068cde6f67135ad
thetylerace13 <tylergemora@yahoo.com>
2016-11-21 13:31:55 -0800

    Made new scene for title screen and put buttons (currently non-functional).

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    public void clickStart()
+    {
+        Debug.Log("Start");
+    }
+
+    public void clickQuit()
+    {
+        Debug.Log("Quit");
+    }
+}
